1904d524d0d2c3a848602db7c84473c94e3f5b50
Iaannn06 <alfianazad05@gmail.com>
2024-03-08 12:57:14 +0700

    Add project files.

---
+++ praktikum1algoritma/praktikum1algoritma.cpp
@@ -0,0 +1,8 @@
+// praktikum1algoritma.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+// AlgoritmaPraktikum1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
